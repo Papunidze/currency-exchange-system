05033cee89ac96c56e32c83df971d6836485bbfc
papunidze <papunidze07@gmail.com>
2025-02-18 05:16:52 -0800

    feat(input): add placeholder support and improve styling for input component

--- shared/ui/input/input.tsx
+++ shared/ui/input/input.tsx
@@ -1,21 +1,27 @@
-import React, { useId } from "react";
-import styles from "./input.module.scss";
-import { InputProps } from "@app-shared/interfaces";
+import React, { useId } from 'react';
+import styles from './input.module.scss';
+import { InputProps } from '@app-shared/interfaces';
 
 const Input = ({
-  label = "Enter Text...",
+  label = 'Enter Text...',
   startContent,
   endContent,
+  placeholder = '',
   ...props
 }: InputProps) => {
   const uniqueId = useId();
 
   return (
-    <div className={styles.inputWrapper}>
+    <div className={styles.input__wrapper}>
       {startContent && (
         <div className={styles.input__start}>{startContent}</div>
       )}
-      <input className={styles.input__field} {...props} id={uniqueId} />
+      <input
+        className={styles.input__field}
+        {...props}
+        id={uniqueId}
+        placeholder={placeholder}
+      />
       <label className={styles.input__label} htmlFor={uniqueId}>
         {label}
       </label>
